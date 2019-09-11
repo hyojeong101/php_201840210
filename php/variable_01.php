96e12f2f3fc8cdc314905262e417ebc52d875940
hyojeong <aaa8132@naver.com>
2019-09-11 14:10:30 +0900

    variable_test

---
+++ php/variable_01.php
@@ -0,0 +1,14 @@
+<?php
+$age = 17;
+var_dump($age);
+
+//is_int($age);
+//is_string($age);
+
+//const
+//define()
+
+$temp = 20.5;
+$name = "현재 온도는 $temp 입니다.";
+
+echo $name;
---
+++ script/variable_01.js
@@ -0,0 +1,6 @@
+let temp = 20.5;
+const message = "현재 온도는 ${temp}입니다."
+
+
+console.log(message);
+
